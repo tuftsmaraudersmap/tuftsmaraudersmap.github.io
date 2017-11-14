4e46dbdf5f5ea26ad9c2001be76c1c0efbbd129e
tuftsmaraudersmap <william.chupp@dot.gov>
2017-11-13 22:35:40 -0500

    adding voice module

--- voice.js
+++ voice.js
@@ -1,15 +1,15 @@
 navigator.mediaDevices.getUserMedia({audio:true})
 	.then(stream => {
-		rec = new MediaRecorder(stream);
+		rec = new MediaRecorder(stream, {mimeType:"audio/webn;codec=flac"});
 		rec.ondataavailable = e => {
 			audioChunks.push(e.data);
 			if (rec.state == "inactive"){
-        let blob = new Blob(audioChunks,{type:'audio/x-mpeg-3'});
+        let blob = new Blob(audioChunks,{type:'audio/webn;codecs=flac'});
         console.log(blob.type);
        	console.log(URL.createObjectURL(blob));
 
-     }
-		}
+     	}
+	}
 	})
 	.catch(e=>console.log(e));
   
